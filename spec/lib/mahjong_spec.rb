6a2a724e15e872331a4d21cc515ab9b995ce2b41
sue445 <sue445@sue445.net>
2015-02-10 15:51:56 +0900

    Refactor: add rspec-parametrized

--- spec/lib/mahjong_spec.rb
+++ spec/lib/mahjong_spec.rb
@@ -2,7 +2,15 @@ describe "Mahjong" do
   let(:tehai){ "七七七②③④⑧⑧４５６①①①" }
 
   describe "#agari?" do
-    it_is_asserted_by{ Mahjong.agari?(tehai) == true }
+    using RSpec::Parameterized::TableSyntax
+
+    where(:tehai, :expected) do
+      "七七七②③④⑧⑧４５６①①①" | true
+    end
+
+    with_them do
+      it_is_asserted_by{ Mahjong.agari?(tehai) == true }
+    end
   end
 
   describe "#kootu_count" do
