f70c74b3d6f6cee37fefa6635383fadb82d5198f
sue445 <sue445@sue445.net>
2015-02-10 16:06:29 +0900

    Add test case

--- spec/lib/mahjong_spec.rb
+++ spec/lib/mahjong_spec.rb
@@ -6,9 +6,14 @@ describe "Mahjong" do
 
     where(:tehai, :expected) do
       "七七七②③④⑧⑧４５６①①①" | true
+      "一二三②③④⑧⑧４５６①①①" | true
     end
 
     with_them do
+      before do
+        # debug
+        puts "手牌:#{tehai}, 刻子 #{Mahjong.kootu_count(tehai)}, 雀頭 #{Mahjong.jantou_count(tehai)}"
+      end
       it_is_asserted_by{ Mahjong.agari?(tehai) == true }
     end
   end
